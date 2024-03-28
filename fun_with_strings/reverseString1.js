af1df40dbd0b7043c0d9c787dc5076feaed0c5da
ltorre701 <142362616+ltorre701@users.noreply.github.com>
2024-03-28 00:28:28 +0000

    created the fun with strings folder

---
+++ fun_with_strings/reverseString1.js
@@ -0,0 +1,11 @@
+// intiliaze a variable called someStr to some string of your choice
+let someStr = "some string of your choice"
+// create a named function that takes an argument `str`
+function reverseString(str) {
+    // in the function reverse `str`
+    str = str.split('').reverse().join('');
+    // return `str`
+    return reverse;
+}
+// outside of the function Log the function and pass someStr
+console.log(someStr(someStr));
