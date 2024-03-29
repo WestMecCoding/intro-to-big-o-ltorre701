b8646bfe52eb20eb28cac1aaf408f247c3fd1e48
ltorre701 <142362616+ltorre701@users.noreply.github.com>
2024-03-29 00:26:46 +0000

    added the revTimeTest file

---
+++ fun_with_strings/revStrFor.js
@@ -0,0 +1,70 @@
+let someStr;
+let charCount;
+function revStrFor(str) {
+    let revStr = "";
+    for(let i= str.length - 1; i >= 0; i--) {
+        revStr += str[i];
+    }
+    return revStr;
+}
+function buildStr(n) {
+    let str = "";
+    for (let i = 1; i <= n; i++) {
+        str += "a";
+    }
+    return str;
+}
+
+charCount = 10;
+someStr = buildStr(charCount);
+console.time(`for loop reverse string ${charCount}`);
+revStrFor(someStr);
+console.timeEnd(`for loop reverse string ${charCount}`);
+
+charCount = 100;
+someStr = buildStr(charCount);
+console.time(`for loop reverse string ${charCount}`);
+revStrFor(someStr);
+console.timeEnd(`for loop reverse string ${charCount}`);
+
+charCount = 1000;
+someStr = buildStr(charCount);
+console.time(`for loop reverse string ${charCount}`);
+revStrFor(someStr);
+console.timeEnd(`for loop reverse string ${charCount}`);
+
+charCount = 10000;
+someStr = buildStr(charCount);
+console.time(`for loop reverse string ${charCount}`);
+revStrFor(someStr);
+console.timeEnd(`for loop reverse string ${charCount}`);
+
+charCount = 100000;
+someStr = buildStr(charCount);
+console.time(`for loop reverse string ${charCount}`);
+revStrFor(someStr);
+console.timeEnd(`for loop reverse string ${charCount}`);
+
+charCount = 1000000;
+someStr = buildStr(charCount);
+console.time(`for loop reverse string ${charCount}`);
+revStrFor(someStr);
+console.timeEnd(`for loop reverse string ${charCount}`);
+
+charCount = 10000000;
+someStr = buildStr(charCount);
+console.time(`for loop reverse string ${charCount}`);
+revStrFor(someStr);
+console.timeEnd(`for loop reverse string ${charCount}`);
+
+charCount = 100000000;
+someStr = buildStr(charCount);
+console.time(`for loop reverse string ${charCount}`);
+revStrFor(someStr);
+console.timeEnd(`for loop reverse string ${charCount}`);
+
+charCount = 1000000000;
+someStr = buildStr(charCount);
+console.time(`for loop reverse string ${charCount}`);
+revStrFor(someStr);
+console.timeEnd(`for loop reverse string ${charCount}`);
